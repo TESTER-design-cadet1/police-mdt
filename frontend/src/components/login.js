9d42a3b84bccf7f2784e379056fb3950d09d45aa
TESTER-design-cadet1 <phillipsh118@hwbcymru.net>
2025-06-16 11:06:04 +0100

    Create login.js

---
+++ frontend/src/components/login.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+function Login({ onLogin }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:4000/api/auth/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, password })
+    });
+    const data = await response.json();
+    if (data.token) {
+      onLogin(data.token, data.user);
+    } else {
+      alert(data.error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <input type="text" placeholder="Username" value={username} onChange={e => setUsername(e.target.value)} /><br />
+      <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} /><br />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
+
+export default Login;
+export default Login;
